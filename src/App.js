1b68cc0242a4190b467e2789c5f9b5fe06a4baea
Arjun N <arjunn3007@gmail.com>
2021-08-09 18:52:12 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
+
+import Icon from './components/icons';
+import {ToastContainer,toast} from 'react-toastify'
+import {Card,CardBody,Container,Button,Col,Row} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+const itemArray = new Array(9).fill("empty")
+
+const reloadGame = () =>{
+
+}
+const checkIsWinner = () => {
+
+}
+const changeItem = (itemNumber) => {
+
+}
+const App = () => {
+  const  [isCross,setIsCross] = useState(false)
+  const [winMessage,setWinMessage] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container className="p-5">
+     <ToastContainer position='bottom-center'/>
+     <Row>
+       <Col md={6} className="offset-md-3">
+         <div className="grid">
+           {itemArray.map((item,index)=>(
+             <Card>
+               <CardBody className="box">
+                 <Icon name={item} />
+               </CardBody>
+             </Card>
+           ))}
+         </div>
+
+       </Col>
+     </Row>
+   </Container>
   );
 }
 
